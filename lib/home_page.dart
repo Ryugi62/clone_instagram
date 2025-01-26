9de80480d08ccdf70a2daf8f411fe94a2bba9012
Ryugi62 <xorjf1027@naver.com>
2025-01-26 15:21:57 +0900

    feat: 피드 스크린 경로 수정 및 프로필 스크린 추가

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:clone_instagram/feed_screen.dart';
+import 'package:clone_instagram/screens/feed_screen.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
